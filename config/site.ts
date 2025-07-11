48b5cb4d016186abeabdd4bef651a7e150a97693
Daniel D <daniel.elohi326@gmail.com>
2025-07-11 08:07:01 +0530

    Next.js Installation

---
+++ config/site.ts
@@ -0,0 +1,53 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Doodle UI",
+  description: "A copy-paste ready design components for modern web development.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/"
+    }
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/MrDanielD326/DoodleUI",
+    twitter: "https://twitter.com/hero_ui",
+    docs: "https://drive.google.com/file/d/1qNgV47XwvNDoaeiIMV-iO6mslZu-r1W8/view",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://www.linkedin.com/in/danield326",
+  },
+};
